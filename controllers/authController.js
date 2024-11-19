f6eead9c83fa25ac120d254361a19c9230b263d7
Sameer Khan <sameerkhann241@gmail.com>
2024-11-19 23:40:33 +0530

    Cookies setting changed

--- controllers/authController.js
+++ controllers/authController.js
@@ -63,8 +63,8 @@ export const login = async (req, res) => {
         // console.log("Token Created")
 
         // Set token in cookie
-        res.cookie("authToken", token, { httpOnly: true,  sameSite: "none", maxAge: 7 * 24 * 60 * 60 * 1000 });
-        // secure: true,
+        res.cookie("authToken", token, { httpOnly: true, secure: true, sameSite: "None", maxAge: 7 * 24 * 60 * 60 * 1000 });
+        
 
         // console.log("Cookies set")
 
